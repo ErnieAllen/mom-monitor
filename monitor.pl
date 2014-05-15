6c8285ce4bc07bfbdeb461e12eed6992debd8a64
Ernie Allen <eallen@redhat.com>
2014-05-15 11:13:19 -0400

    Send messages containing disk usage and errors

--- monitor.pl
+++ monitor.pl
@@ -21,14 +21,30 @@
 use strict;
 use warnings;
 use Getopt::Std;
-use POSIX;
 use Filesys::DfPortable;
-use File::Slurp;
+use Data::Dumper;
+use File::ReadBackwards;
 
 use qpid_proton;
 
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
+sub HELP_MESSAGE() {
+    print "Usage: monitor.pl [OPTIONS] -a <ADDRESS>\n";
+    print "Options:\n";
+#    print "\t-c        - Config file (\$HOME/.config/monitor/monitor.conf)\n";
+    print "\t-d        - Directory to check for size (/)\n";
+    print "\t-a        - Address amqp://<domain>[/<name>] (amqp://0.0.0.0)\n";
+    print "\t-h        - This help message\n";
+    exit(0);
+}
+
 my $msg;
+my %options = ();
+getopts("c:d:a:h:", \%options) or HELP_MESSAGE();
+
+my $address = $options{a} || "amqp://0.0.0.0";
+my $subject = "Computer status messages";
+my $dir = $options{d} || "/";
 
 sub construct_messages {
     my @messages;
@@ -36,11 +52,8 @@ sub construct_messages {
     # get the percent disk full
     push(@messages, get_disk_info());
 
-    # get the virus / malware scan info
-    push(@messages, get_scan_info());
-
-    # get the percent backup disk full
-    push(@messages, get_backup_info());
+    # get the log file errors
+    push(@messages, get_log_info());
 
     return @messages;
 }
@@ -49,72 +62,49 @@ sub construct_messages {
 sub get_disk_info {
     $msg = new qpid::proton::Message();
 
-    my $ref = dfportable("C:/", 1024);  # output is 1K blocks
+    my $ref = dfportable($dir, 1024);  # output is 1K blocks
 
     if (defined($ref)) {
-        $msg->set_body({"disk" => {"size" => $ref->{blocks}, "used" => $ref->{used}}}, qpid::proton::MAP);
+        $msg->set_body({"disk" => {"size" => $ref->{blocks}, "used" => $ref->{bavail}}}, qpid::proton::MAP);
     } else {
-        $msg->set_body({"disk" => {"size" => "", "used" => "", "error" => "Error getting disk info"}}, qpid::proton::MAP);
+        $msg->set_body({"disk" => {"size" => "", "used" => "", "error" => "Error getting disk info for $dir"}}, qpid::proton::MAP);
     }
     # each datum has its own version in case the format changes
     $msg->get_annotations->{"version"} = 0.1;
+#print Dumper($msg->get_body());
+
     return $msg;
 } 
 
-# send a message containing a hash with the contents of a file
-sub get_scan_info {
-    $msg = new qpid::proton::Message();
+# grep through some log files looking for the word "fail" and send the last matching lines back 
+sub get_log_info {
 
-    # location of log file on Vista
-    # TODO: read the file name from a config file
-    my $log_file = "/home/ernie/scan/results.txt";
-    my $scan_file_contents = read_file($log_file, err_mode => 'quiet' ); 
-    if ($scan_file_contents) {
-        $msg->set_body({"scan" => {"date" => strftime("%Y-%m-%d %H:%M:%S", localtime(time)), "results" => $scan_file_contents}}, qpid::proton::MAP);
-    } else {
-        $msg->set_body({"scan" => {"date" => strftime("%Y-%m-%d %H:%M:%S", localtime(time)), "results" => "", "error" => "error reading file"}}, qpid::proton::MAP);
- 
-    }
+    my $log_dir = "/var/log";
+    my @log_files = qw/messages wtmp dmesg/;
+	my $lim = 100; # read last 100 lines of the files
+
+	my %fails = ();
+    $msg = new qpid::proton::Message();
+	foreach my $name (@log_files) {
+
+	    my $bw = File::ReadBackwards->new( "$log_dir/$name" );
+		if ( $bw ) {
+
+			while( defined( my $line = $bw->readline ) ) {
+				if ($line =~ /fail/i ) {
+					push @{$fails{ $name }}, $line;
+					last if --$lim <= 0;
+				}
+			}
+		}
+	}
+#print Dumper(\%fails);
+	$msg->set_body({"logs" => \%fails}, qpid::proton::MAP);
     $msg->get_annotations->{"version"} = 0.1;
     return $msg;
+
 }
 
-# send a message containing a hash with the system's disk info
-sub get_backup_info {
-    $msg = new qpid::proton::Message();
-
-    my $ref = dfportable("D:/", 1024);  # output is 1K blocks
-
-    if (defined($ref)) {
-        $msg->set_body({"backup" => {"size" => $ref->{blocks}, "used" => $ref->{used}}}, qpid::proton::MAP);
-    } else {
-        $msg->set_body({"backup" => {"size" => "", "used" => "", error => "Error getting backup drive info"}}, qpid::proton::MAP);
-    }
-    # each datum has its own version in case the format changes
-    $msg->get_annotations->{"version"} = 0.1;
-    return $msg;
-} 
-
-sub VERSION_MESSAGE() {
-}
-
-sub HELP_MESSAGE() {
-    print "Usage: monitor.pl [OPTIONS] -a <ADDRESS>\n";
-    print "Options:\n";
-    print "\t-c        - Config file (\$HOME/.config/monitor/monitor.conf)\n";
-    print "\t-d        - Disk to check for size (C:/)\n";
-    print "\t-b        - Backup disk to check for size (D:/)\n";
-    print "\t-s        - Scan file to return (\$HOME/scan/results.txt)\n";
-    print "\t-h        - this help message\n";
-    print "\t<ADDRESS> - amqp://<domain>[/<name>]\n";
-    exit(0);
-}
-
-my %options = ();
-getopts("a:C:s:h:", \%options) or HELP_MESSAGE();
-
-my $address = $options{a} || "amqp://0.0.0.0";
-my $subject = "Computer status messages";
 
 my $messenger = new qpid::proton::Messenger();
 
